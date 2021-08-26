401c737e3d6fe1931c866914335c121acf3f0023
bajrachar <ravi.bajracharya@wiseyak.com>
2021-08-26 16:43:39 +0545

    fixed project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject wikidata-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure application and tools for extraction of biomedical concepts and relationships from wikidata rdf file."
+  :url "https://github.com/datummd/wikidata-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
